17dd2716191d460f139af9d9feac7e1266e17d47
Jonas Büchner <JonasBuchner36@gmail.com>
2025-02-06 12:46:12 +0100

    Demo klasse oprettet med prob

---
+++ MyFirstGitHubClassRoomConsoleApp60/demo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    internal class demo
+    {
+        public string name {  get; set; }
+        public string description { get; set; }
+        public string title { get; set; }
+    }
+}
